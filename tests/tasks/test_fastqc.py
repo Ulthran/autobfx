f8797fe360171bb0ddde5446697dda9824294a36
Ulthran <ctbushman@gmail.com>
2024-11-01 18:41:16 -0400

    Working task (no mock)

--- tests/tasks/test_fastqc.py
+++ tests/tasks/test_fastqc.py
@@ -1,13 +1,17 @@
+import os
 from pathlib import Path
 from autobfx.tasks.fastqc import run_fastqc
 
 
 def test_fastqc(data_fp: Path, dummy_project_fp: Path):
+    os.makedirs(dummy_project_fp / "fastqc", exist_ok=True)
+    os.makedirs(dummy_project_fp / "logs" / "fastqc", exist_ok=True)
+
     assert (
         run_fastqc.fn(
-            input_fp=data_fp / "reads",
-            output_fp=dummy_project_fp / "fastqc",
-            log_fp=dummy_project_fp / "logs",
+            input_fp=data_fp / "reads" / "LONG_R1.fastq.gz",
+            output_fp=dummy_project_fp / "fastqc" / "LONG_R1.fastq.gz",
+            log_fp=dummy_project_fp / "logs" / "fastqc" / "LONG_R1.fastq.gz.log",
         )
-        == dummy_project_fp / "fastqc"
+        == dummy_project_fp / "fastqc" / "LONG_R1.fastq.gz"
     )
