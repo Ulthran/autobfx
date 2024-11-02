7f320b7293dc603e32b8bd1e250c6daf817f9c29
Ulthran <ctbushman@gmail.com>
2024-11-02 13:30:57 -0400

    Mocking task tests

--- tests/tasks/test_fastqc.py
+++ tests/tasks/test_fastqc.py
@@ -1,17 +1,28 @@
 import os
 from pathlib import Path
+from autobfx.lib.utils import check_already_done
 from autobfx.tasks.fastqc import run_fastqc
 
 
-def test_fastqc(data_fp: Path, dummy_project_fp: Path):
-    os.makedirs(dummy_project_fp / "fastqc", exist_ok=True)
-    os.makedirs(dummy_project_fp / "logs" / "fastqc", exist_ok=True)
+def test_fastqc(data_fp: Path, dummy_project_fp: Path, mocker):
+    mocker.patch("prefect_shell.ShellOperation.run", return_value="FASTQC MOCKED")
+
+    input_fp = data_fp / "reads" / "LONG_R1.fastq.gz"
+    output_fp = dummy_project_fp / "fastqc" / "LONG_R1.fastq.gz"
+    log_fp = dummy_project_fp / "logs" / "fastqc" / "LONG_R1.fastq.gz.log"
+
+    os.makedirs(output_fp.parent, exist_ok=True)
+    os.makedirs(log_fp.parent, exist_ok=True)
 
     assert (
         run_fastqc.fn(
-            input_fp=data_fp / "reads" / "LONG_R1.fastq.gz",
-            output_fp=dummy_project_fp / "fastqc" / "LONG_R1.fastq.gz",
-            log_fp=dummy_project_fp / "logs" / "fastqc" / "LONG_R1.fastq.gz.log",
+            input_fp=input_fp,
+            output_fp=output_fp,
+            log_fp=log_fp,
         )
-        == dummy_project_fp / "fastqc" / "LONG_R1.fastq.gz"
+        == output_fp
     )
+
+    assert check_already_done(output_fp)
+    with open(log_fp) as f:
+        assert f.read() == "FASTQC MOCKED"
