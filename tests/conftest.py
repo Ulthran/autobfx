d20dab3eed00c9a8e803f7dd772dd21b58096705
Ulthran <ctbushman@gmail.com>
2024-11-02 15:04:49 -0400

    DEBUG

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,7 @@ import os
 import pytest
 import sys
 from pathlib import Path
-from autobfx.scripts.init import main as Init
+#from autobfx.scripts.init import main as Init
 
 # Ensure 'src' is in the Python path for package imports
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
@@ -12,7 +12,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../s
 def data_fp() -> Path:
     return (Path(__file__).parent / "data").resolve()
 
-
+"""
 @pytest.fixture()
 def dummy_project_fp(data_fp: Path, tmp_path: Path) -> Path:
     project_fp = tmp_path / "projects" / "test"
@@ -21,3 +21,4 @@ def dummy_project_fp(data_fp: Path, tmp_path: Path) -> Path:
     config.config_to_yaml(project_fp / "config.yaml")
 
     return project_fp
+"""
