c4641e8a5fcf748ac9e191343ca16a4f9a5f7e7e
Ulthran <ctbushman@gmail.com>
2024-11-20 15:43:22 -0500

    Remove unneeded CI workflow

--- tests/e2e/test_clean_shotgun.py
+++ tests/e2e/test_clean_shotgun.py
@@ -1,1 +1,3 @@
-# TODO: Run full clean_shotgun test flow and make sure it works
+def test_clean_shotgun(dummy_project, test_runner):
+    pass
+    # TODO: Run full clean_shotgun test flow and make sure it works
